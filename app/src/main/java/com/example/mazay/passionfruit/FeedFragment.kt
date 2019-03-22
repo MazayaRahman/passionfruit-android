ba36c701d05c9b5a6b9cce3a21a07edba555683e
Mazaya Rahman <mazaya.rahman@gmail.com>
2019-03-21 21:50:48 -0400

    attempting to fetch data from feed viewmodel and display it in the recview from feed fragment

--- app/src/main/java/com/example/mazay/passionfruit/FeedFragment.kt
+++ app/src/main/java/com/example/mazay/passionfruit/FeedFragment.kt
@@ -1,8 +1,58 @@
 package com.example.mazay.passionfruit
 
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProvider
+import android.arch.lifecycle.ViewModelProviders
+import android.os.Bundle
+import android.support.annotation.Nullable
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import com.example.mazay.passionfruit.models.FeedResponse
+import com.example.mazay.passionfruit.models.ProfileResponse
+import com.example.mazay.passionfruit.service.apiClient
+import com.example.mazay.passionfruit.viewmodels.FeedViewModel
+import kotlinx.android.synthetic.main.feed_layout.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import android.widget.Toast
+import android.provider.ContactsContract.CommonDataKinds.Note
+
+
+
 
 class FeedFragment: Fragment() {
+    //var userList: MutableList<ProfileResponse.User>? = mutableListOf()
+    private lateinit var model: FeedViewModel
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        retainInstance = true
+
+
+        model = ViewModelProviders.of(this).get(FeedViewModel::class.java)
+
+        model.getFeed()?.observe(this, Observer { users->
+            recyclerView.apply {
+                // set a LinearLayoutManager to handle Android
+                layoutManager = LinearLayoutManager(context, LinearLayout.VERTICAL, false)
+                // set the custom adapter to the RecyclerView
+                adapter = CustomAdapter(users)
+            }
+        })
+
+    }
+
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? =
+            inflater.inflate(R.layout.feed_layout, container, false)
 
 }
--- app/src/main/java/com/example/mazay/passionfruit/viewmodels/FeedViewModel.kt
+++ app/src/main/java/com/example/mazay/passionfruit/viewmodels/FeedViewModel.kt
@@ -1,12 +1,61 @@
 package com.example.mazay.passionfruit.viewmodels
 
 import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
+import android.util.Log
+import com.example.mazay.passionfruit.models.FeedResponse
 import com.example.mazay.passionfruit.models.ProfileResponse
+import com.example.mazay.passionfruit.service.apiClient
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 
 class FeedViewModel: ViewModel() {
+    var users: MutableLiveData<List<ProfileResponse.User>>? = null
 
-    private val users: LiveData<List<ProfileResponse.User>>? = null
+
+    fun getFeed() : MutableLiveData<List<ProfileResponse.User>>?{
+        val retrofit = Retrofit.Builder()
+                .baseUrl("https://api.myjson.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+
+        val apiClientService = retrofit.create(apiClient::class.java)
+
+        apiClientService.getUsers().enqueue(object: Callback<FeedResponse> {
+
+            override fun onResponse(call: Call<FeedResponse>, response: Response<FeedResponse>) {
+                if (!response.isSuccessful) {
+                    // Report error here
+                    Log.d("MainActivity", "feedResponse failed!")
+                    return
+                }
+                val feedResponse = response.body()
+                        ?: // Report error here
+                        return
+                //Log.d("MainActivity", "userdata: " +(feedResponse?.users==null))
+
+                if(feedResponse.users != null) {
+                    //Log.d("MainActivity", "feedresponse: " +feedResponse.toString())
+                    users?.setValue(response.body()!!.users);
+
+                }
+            }
+            override fun onFailure(call: Call<FeedResponse>, t: Throwable) {
+                // Report error here
+                Log.d("MainActivity", t.toString())
+            }
+        });
+
+        return users
+    }
+
+
+
 
 
 }
