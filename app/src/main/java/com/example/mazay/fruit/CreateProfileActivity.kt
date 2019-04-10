b7d05d30aafee481ae1fa48eaf1c2298c58ec130
Mazaya Rahman <mazaya.rahman@gmail.com>
2019-04-09 23:35:29 -0400

    login working properly

---
+++ app/src/main/java/com/example/mazay/fruit/CreateProfileActivity.kt
@@ -0,0 +1,13 @@
+package com.example.mazay.fruit
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_profile)
+
+    }
+}
