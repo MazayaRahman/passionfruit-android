232569bc206bf036756abcb70135e4244d990cf6
Mazaya Rahman <mazaya.rahman@gmail.com>
2019-04-15 23:38:24 -0400

    gallery intent working

--- app/src/main/java/com/example/mazay/fruit/ProfileFragment.kt
+++ app/src/main/java/com/example/mazay/fruit/ProfileFragment.kt
@@ -3,12 +3,15 @@ package com.example.mazay.fruit
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProvider
 import android.arch.lifecycle.ViewModelProviders
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import com.example.mazay.fruit.viewmodels.ProfileViewModel
 import kotlinx.android.synthetic.main.profile_layout.*
 
@@ -16,9 +19,11 @@ class ProfileFragment: Fragment() {
 
     private lateinit var model: ProfileViewModel
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         retainInstance = true
+
 
         model = ViewModelProviders.of(this).get(ProfileViewModel::class.java)
         //model.setUser()
@@ -28,12 +33,30 @@ class ProfileFragment: Fragment() {
         })
 
 
+
+
+
     }
 
-    override fun onCreateView(inflater: LayoutInflater,
-                              container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? =
-            inflater.inflate(R.layout.profile_layout, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        // Inflate the layout for this fragment
+        val v = inflater.inflate(R.layout.profile_layout, container, false)
+
+
+        val editprofile_btn = v.findViewById(R.id.edit_profile) as Button
+        // set on-click listener
+        editprofile_btn.setOnClickListener {
+            // your code to perform when the user clicks on the button
+            val intent : Intent = Intent(context, CreateProfileActivity::class.java)
+            //intent.putExtra("user",user as Serializable)
+            startActivity(intent)
+        }
+
+        return v
+
+    }
+
 
 
 }
